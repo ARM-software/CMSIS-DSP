93c5499ee83f772617ef1ea6cc1875d2f94b4d57
Jonatan Antoni <jonatan.antoni@arm.com>
2022-07-05 11:55:56 +0200

    GH pages with version dropdown
    - Add dropdown box to navigate between versions.
    - Fixup dev drop version
    - Add a script to update the version list.
    - Patching existing documentation to pick up version selector.

--- main/footer.js
+++ main/footer.js
@@ -1,7 +1,7 @@
 function writeHeader() {
-    document.write('Version 1.10.2-dev1');
+    document.write('Version 1.10.2-dev2');
 };
 
 function writeFooter()  {
-    document.write('Generated on Tue Jul  5 2022 06:22:07 for CMSIS-DSP 1.10.2-dev1+g9d88671. Copyright &copy; 2022 Arm Limited (or its affiliates). All rights reserved.');
+    document.write('Generated on Tue Jul  5 2022 09:35:33 for CMSIS-DSP 1.10.2-dev2+ge0e5607. Copyright &copy; 2022 Arm Limited (or its affiliates). All rights reserved.');
 };
--- v1.10.1/footer.js
+++ v1.10.1/footer.js
@@ -3,5 +3,5 @@ function writeHeader() {
 };
 
 function writeFooter()  {
-    document.write('Generated on Tue Jul  5 2022 06:23:42 for CMSIS-DSP 1.10.1. Copyright &copy; 2022 Arm Limited (or its affiliates). All rights reserved.');
+    document.write('Generated on Tue Jul  5 2022 09:29:47 for CMSIS-DSP 1.10.1. Copyright &copy; 2022 Arm Limited (or its affiliates). All rights reserved.');
 };
