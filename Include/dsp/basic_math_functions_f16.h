2770fe4619132d93b9cb412c19797d5be5de8118
Silfurion <nathan@lesbottet.net>
2022-07-14 15:20:22 +0200

    Prepare for merge

--- Include/dsp/basic_math_functions_f16.h
+++ Include/dsp/basic_math_functions_f16.h
@@ -1,8 +1,8 @@
 /******************************************************************************
  * @file     basic_math_functions_f16.h
  * @brief    Public header file for CMSIS DSP Library
- * @version  V1.10.0
- * @date     08 July 2021
+ * @version  V1.10.1
+ * @date     14 July 2022
  * Target Processor: Cortex-M and Cortex-A cores
  ******************************************************************************/
 /*
@@ -158,6 +158,8 @@ void arm_clip_f16(const float16_t * pSrc,
   float16_t low, 
   float16_t high, 
   uint32_t numSamples);
+
+
 
 #endif /* defined(ARM_FLOAT16_SUPPORTED)*/
 
