ba028f654b6fa40833c2915b9a6e874936fa18f6
Vladimir Marchenko <vladimir.marchenko@arm.com>
2022-07-28 11:44:19 +0200

    Updated default version dropdown view. (#35)
    * Updated default version dropdown view.
    
    * Update main documentation
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Christophe Favergeon <48906714+christophe0606@users.noreply.github.com>

--- main/footer.js
+++ main/footer.js
@@ -3,5 +3,5 @@ function writeHeader() {
 };
 
 function writeFooter()  {
-    document.write('Generated on Thu Jul 28 2022 09:38:40 for CMSIS-DSP 1.10.2-dev18+g5d43a37. Copyright &copy; 2022 Arm Limited (or its affiliates). All rights reserved.');
+document.write('Generated on Thu Jul 28 2022 09:38:40 for CMSIS-DSP 1.10.2-dev18+g5d43a37. Copyright &copy; 2022 Arm Limited (or its affiliates). All rights reserved.');
 };
