db5a9a92fa8d591a53b8c3b11a49800d35646e48
AlbertHuang-CPU <Albert.Huang@armchina.com>
2025-03-21 08:02:50 +0100

    fix a small bug of the copying history samples in taps_5-8 processing

--- Source/FilteringFunctions/arm_fir_f32.c
+++ Source/FilteringFunctions/arm_fir_f32.c
@@ -348,7 +348,7 @@ __STATIC_INLINE void arm_fir_f32_5_8_mve(const arm_fir_instance_f32 * S,
     pTempSrc = &pState[blockSize];
     pTempDest = pState;
     blkCnt = numTaps;
-    while (blkCnt > 0)
+    while (blkCnt > 1)
     {
         *pTempDest++ = *pTempSrc++;
         blkCnt--;
