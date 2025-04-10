f5acf12580280acb2676b8c051f83b419d5fdc26
Christophe Favergeon <christophe.favergeon@gmail.com>
2025-04-10 07:32:11 +0200

    Small changes to build PythonWrapper on darwin

--- PythonWrapper/build_darwin/create.sh
+++ PythonWrapper/build_darwin/create.sh
@@ -2,7 +2,7 @@ cmake -DHOST=YES \
   -DLOOPUNROLL=ON \
   -DWRAPPER=YES \
   -DCMAKE_POSITION_INDEPENDENT_CODE=YES \
-  -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+  -DCMAKE_OSX_ARCHITECTURES="arm64" \
   -DCMAKE_OSX_DEPLOYMENT_TARGET="10.9" \
   -DCMSISDSP="path to CMSIS-DSP folder" \
   -DCMAKE_C_FLAGS_RELEASE="-std=c11 -Ofast -ffast-math -DNDEBUG -Wall -Wextra" \
@@ -12,3 +12,8 @@ cmake -DHOST=YES \
 
 # For Mac universal lib
 # -arch x86_64 -arch arm64 -mmacosx-version-min=11.0
+
+# Since the extension is now built with Neon,
+# arm64;x86_64 is changed to arm64 only in the OSX
+# architectures
+# -DCMAKE_OSX_ARCHITECTURES="arm64"
