6f5bb5bcf79dec4ea0605f8efbbc0a7d5fefc7c8
felhub <felhub@net4us.at>
2026-06-01 17:46:48 +0200

    fixed "pip install ." for python 3.14.2 on Mint 22.3

--- setup.py
+++ setup.py
@@ -196,8 +196,8 @@ class BuildCMSISDSPExtensions(build_ext):
       "-DWRAPPER=YES",
       "-DLOOPUNROLL=ON",
       "-DCMAKE_BUILD_TYPE=Release",
-      '-DCMAKE_C_FLAGS_RELEASE="-std=c11 -O3 -ffast-math -DNDEBUG -Wall -Wextra"',
-      '-DCMAKE_CXX_FLAGS_RELEASE="-std=c++11 -O3 -ffast-math -DNDEBUG -Wall -Wextra -Wno-unused-parameter"'
+      "-DCMAKE_C_FLAGS_RELEASE=-std=c11 -O3 -ffast-math -DNDEBUG -Wall -Wextra",
+      "-DCMAKE_CXX_FLAGS_RELEASE=-std=c++11 -O3 -ffast-math -DNDEBUG -Wall -Wextra -Wno-unused-parameter"
     ]
 
     if sys.platform != 'win32':
