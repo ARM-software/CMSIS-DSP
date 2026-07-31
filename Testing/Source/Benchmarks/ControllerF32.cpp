ec1bb75277d3e23e2fa7b8819dc734a7926b0bfb
shazha01 <76190761+shazha01@users.noreply.github.com>
2026-07-31 13:40:03 +0800

    Fix some issues in Controller benchmark testing. (#325)
    1. Uninitialized this->pOuta and this->pOutb may lead to the undefined behavior.
    2. Wrong offset of Ib (this->nbSamples << 2) will access sample memory beyond this->nbSamples in ControllerF32.cpp.
    3. Refine code style of ControllerF32.cpp and ControllerQ31.cpp.

--- Testing/Source/Benchmarks/ControllerF32.cpp
+++ Testing/Source/Benchmarks/ControllerF32.cpp
@@ -1,96 +1,120 @@
 #include "ControllerF32.h"
 #include "Error.h"
 
-   
+
     void ControllerF32::test_pid_f32()
     {
        for(int i=0; i < this->nbSamples; i++)
        {
           *this->pDst++ = arm_pid_f32(&instPid, *this->pSrc++);
        }
-    } 
+    }
 
-    void ControllerF32::test_clarke_f32() 
+    void ControllerF32::test_clarke_f32()
     {
        float32_t * Ia;
        float32_t * Ib;
- 
+       float32_t * pOuta;
+       float32_t * pOutb;
+
        Ia = this->pSrc;
-       Ib = this->pSrc + (this->nbSamples << 2);
+       Ib = this->pSrc + this->nbSamples;
+       pOuta = this->pOuta;
+       pOutb = this->pOutb;
        for(int i=0; i < this->nbSamples; i++)
        {
-         arm_clarke_f32(*Ia++,*(--Ib),this->pOuta,this->pOutb);
+         arm_clarke_f32(*Ia++,*(--Ib),pOuta++,pOutb++);
        }
     }
 
-    void ControllerF32::test_inv_clarke_f32() 
+    void ControllerF32::test_inv_clarke_f32()
     {
        float32_t * Ia;
        float32_t * Ib;
- 
+       float32_t * pOuta;
+       float32_t * pOutb;
+
        Ia = this->pSrc;
-       Ib = this->pSrc + (this->nbSamples << 2);
+       Ib = this->pSrc + this->nbSamples;
+       pOuta = this->pOuta;
+       pOutb = this->pOutb;
        for(int i=0; i < this->nbSamples; i++)
        {
-         arm_inv_clarke_f32(*Ia++,*(--Ib),this->pOuta,this->pOutb);
+         arm_inv_clarke_f32(*Ia++,*(--Ib),pOuta++,pOutb++);
        }
     }
 
-    void ControllerF32::test_park_f32() 
+    void ControllerF32::test_park_f32()
     {
        float32_t * Ia;
        float32_t * Ib;
+       float32_t * pOuta;
+       float32_t * pOutb;
 
        Ia = this->pSrc;
-       Ib = this->pSrc + (this->nbSamples << 2);
+       Ib = this->pSrc + this->nbSamples;
+       pOuta = this->pOuta;
+       pOutb = this->pOutb;
        for(int i=0; i < this->nbSamples; i++)
-       {         
-	 arm_park_f32(*Ia++,*(--Ib),this->pOuta,this->pOutb,0.4539905,0.8910065); //theta = 27(Deg) 
+       {
+         arm_park_f32(*Ia++,*(--Ib),pOuta++,pOutb++,0.4539905,0.8910065); //theta = 27(Deg)
        }
     }
 
-    void ControllerF32::test_inv_park_f32() 
+    void ControllerF32::test_inv_park_f32()
     {
        float32_t * Ia;
        float32_t * Ib;
+       float32_t * pOuta;
+       float32_t * pOutb;
 
        Ia = this->pSrc;
-       Ib = this->pSrc + (this->nbSamples << 2);
+       Ib = this->pSrc + this->nbSamples;
+       pOuta = this->pOuta;
+       pOutb = this->pOutb;
        for(int i=0; i < this->nbSamples; i++)
        { 
-	 arm_inv_park_f32(*Ia++,*(--Ib),this->pOuta,this->pOutb,0.4539905,0.8910065); //theta = 27(Deg) 
+         arm_inv_park_f32(*Ia++,*(--Ib),pOuta++,pOutb++,0.4539905,0.8910065); //theta = 27(Deg)
        }
-    }	
+    }
 
-    void ControllerF32::test_sin_cos_f32() 
+    void ControllerF32::test_sin_cos_f32()
     {
-        float32_t sinVal,cosVal;
-        
+        float32_t * pOuta;
+        float32_t * pOutb;
+
+        pOuta = this->pOuta;
+        pOutb = this->pOutb;
         for(int i=0; i < this->nbSamples; i++)
         {
-           arm_sin_cos_f32(0.1,&sinVal,&cosVal);
+           arm_sin_cos_f32(0.1,pOuta++,pOutb++);
         }
     }
-    
+
     void ControllerF32::setUp(Testing::testID_t id,std::vector<Testing::param_t>& params,Client::PatternMgr *mgr)
     {
        std::vector<Testing::param_t>::iterator it = params.begin();
        this->nbSamples = *it;
 
        samples.reload(ControllerF32::SAMPLES_F32_ID,mgr,this->nbSamples);
-       output.create(this->nbSamples,ControllerF32::OUT_SAMPLES_F32_ID,mgr);
 
        switch(id)
        {
            case TEST_PID_F32_1:
-             arm_pid_init_f32(&instPid,1);
+              output.create(this->nbSamples,ControllerF32::OUT_SAMPLES_F32_ID,mgr);
+              arm_pid_init_f32(&instPid,1);
+              this->pDst=output.ptr();
            break;
 
+           default:
+              /* Allocate enough storage for pOuta and pOutb outputs of the controller functions. */
+              output.create(this->nbSamples * 2,ControllerF32::OUT_SAMPLES_F32_ID,mgr);
+              this->pOuta=output.ptr();
+              this->pOutb=output.ptr() + this->nbSamples;
+           break;
        }
 
        this->pSrc=samples.ptr();
-       this->pDst=output.ptr();
-       
     }
 
     void ControllerF32::tearDown(Testing::testID_t id,Client::PatternMgr *mgr)
--- Testing/Source/Benchmarks/ControllerQ31.cpp
+++ Testing/Source/Benchmarks/ControllerQ31.cpp
@@ -1,98 +1,120 @@
 #include "ControllerQ31.h"
 #include "Error.h"
 
-   
+
     void ControllerQ31::test_pid_q31()
     {
        for(int i=0; i < this->nbSamples; i++)
        {
           *this->pDst++ = arm_pid_q31(&instPid, *this->pSrc++);
        }
-    } 
+    }
 
-    void ControllerQ31::test_clarke_q31() 
+    void ControllerQ31::test_clarke_q31()
     {
        q31_t * Ia;
        q31_t * Ib;
+       q31_t * pOuta;
+       q31_t * pOutb;
 
        Ia = this->pSrc;
-       Ib = reinterpret_cast<q31_t *> (reinterpret_cast<uintptr_t>(this->pSrc) + (this->nbSamples << 2));
+       Ib = this->pSrc + this->nbSamples;
+       pOuta = this->pOuta;
+       pOutb = this->pOutb;
        for(int i=0; i < this->nbSamples; i++)
        {
-         arm_clarke_q31(*Ia++,*(--Ib),this->pOuta,this->pOutb);
+         arm_clarke_q31(*Ia++,*(--Ib),pOuta++,pOutb++);
        }
     }
 
-    void ControllerQ31::test_inv_clarke_q31() 
+    void ControllerQ31::test_inv_clarke_q31()
     {
        q31_t * Ia;
        q31_t * Ib;
+       q31_t * pOuta;
+       q31_t * pOutb;
 
        Ia = this->pSrc;
-       Ib = reinterpret_cast<q31_t *> (reinterpret_cast<uintptr_t>(this->pSrc) + (this->nbSamples << 2));
+       Ib = this->pSrc + this->nbSamples;
+       pOuta = this->pOuta;
+       pOutb = this->pOutb;
        for(int i=0; i < this->nbSamples; i++)
        {
-         arm_inv_clarke_q31(*Ia++,*(--Ib),this->pOuta,this->pOutb);
+         arm_inv_clarke_q31(*Ia++,*(--Ib),pOuta++,pOutb++);
        }
     }
 
-    void ControllerQ31::test_park_q31() 
+    void ControllerQ31::test_park_q31()
     {
        q31_t * Ia;
        q31_t * Ib;
+       q31_t * pOuta;
+       q31_t * pOutb;
 
        Ia = this->pSrc;
-       Ib = reinterpret_cast<q31_t *> (reinterpret_cast<uintptr_t>(this->pSrc) + (this->nbSamples << 2));
+       Ib = this->pSrc + this->nbSamples;
+       pOuta = this->pOuta;
+       pOutb = this->pOutb;
        for(int i=0; i < this->nbSamples; i++)
        {
-         arm_park_q31(*Ia++,*(--Ib),this->pOuta,this->pOutb,0x3a1c5c56,0x720c8074); //theta = 27(Deg)
+         arm_park_q31(*Ia++,*(--Ib),pOuta++,pOutb++,0x3a1c5c56,0x720c8074); //theta = 27(Deg)
        }
     }
 
-    void ControllerQ31::test_inv_park_q31() 
+    void ControllerQ31::test_inv_park_q31()
     {
         q31_t * Ia;
         q31_t * Ib;
+        q31_t * pOuta;
+        q31_t * pOutb;
 
         Ia = this->pSrc;
-        Ib = reinterpret_cast<q31_t *> (reinterpret_cast<uintptr_t>(this->pSrc) + (this->nbSamples << 2));
+        Ib = this->pSrc + this->nbSamples;
+        pOuta = this->pOuta;
+        pOutb = this->pOutb;
         for(int i=0; i < this->nbSamples; i++)
         {
-          arm_inv_park_q31(*Ia++,*(--Ib),this->pOuta,this->pOutb,0x3a1c5c56,0x720c8074);
+          arm_inv_park_q31(*Ia++,*(--Ib),pOuta++,pOutb++,0x3a1c5c56,0x720c8074);
         }
     }
 
-    void ControllerQ31::test_sin_cos_q31() 
+    void ControllerQ31::test_sin_cos_q31()
     {
-        q31_t sinVal,cosVal;
-        
+        q31_t * pOuta;
+        q31_t * pOutb;
+
+        pOuta = this->pOuta;
+        pOutb = this->pOutb;
         for(int i=0; i < this->nbSamples; i++)
         {
-           arm_sin_cos_q31(0xccccccd,&sinVal,&cosVal);
+          arm_sin_cos_q31(0xccccccd,pOuta++,pOutb++);
         }
     }
-    
+
     void ControllerQ31::setUp(Testing::testID_t id,std::vector<Testing::param_t>& params,Client::PatternMgr *mgr)
     {
-
-
        std::vector<Testing::param_t>::iterator it = params.begin();
        this->nbSamples = *it;
 
        samples.reload(ControllerQ31::SAMPLES_Q31_ID,mgr,this->nbSamples);
-       output.create(this->nbSamples,ControllerQ31::OUT_SAMPLES_Q31_ID,mgr);
 
        switch(id)
        {
            case TEST_PID_Q31_1:
+              output.create(this->nbSamples,ControllerQ31::OUT_SAMPLES_Q31_ID,mgr);
               arm_pid_init_q31(&instPid,1);
+              this->pDst=output.ptr();
            break;
 
+           default:
+              /* Allocate enough storage for pOuta and pOutb outputs of the controller functions. */
+              output.create(this->nbSamples * 2,ControllerQ31::OUT_SAMPLES_Q31_ID,mgr);
+              this->pOuta=output.ptr();
+              this->pOutb=output.ptr() + this->nbSamples;
+           break;
        }
 
        this->pSrc=samples.ptr();
-       this->pDst=output.ptr();
-       
     }
 
     void ControllerQ31::tearDown(Testing::testID_t id,Client::PatternMgr *mgr)
